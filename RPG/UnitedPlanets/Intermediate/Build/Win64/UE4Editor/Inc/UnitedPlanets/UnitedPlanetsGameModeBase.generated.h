0774d0524359f25bdc904af0a3fb810669d91f83
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2020-05-09 08:59:52 -0700

    02 - Added Content

---
+++ RPG/UnitedPlanets/Intermediate/Build/Win64/UE4Editor/Inc/UnitedPlanets/UnitedPlanetsGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef UNITEDPLANETS_UnitedPlanetsGameModeBase_generated_h
+#error "UnitedPlanetsGameModeBase.generated.h already included, missing '#pragma once' in UnitedPlanetsGameModeBase.h"
+#endif
+#define UNITEDPLANETS_UnitedPlanetsGameModeBase_generated_h
+
+#define UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_SPARSE_DATA
+#define UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_RPC_WRAPPERS
+#define UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAUnitedPlanetsGameModeBase(); \
+	friend struct Z_Construct_UClass_AUnitedPlanetsGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AUnitedPlanetsGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/UnitedPlanets"), NO_API) \
+	DECLARE_SERIALIZER(AUnitedPlanetsGameModeBase)
+
+
+#define UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAUnitedPlanetsGameModeBase(); \
+	friend struct Z_Construct_UClass_AUnitedPlanetsGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AUnitedPlanetsGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/UnitedPlanets"), NO_API) \
+	DECLARE_SERIALIZER(AUnitedPlanetsGameModeBase)
+
+
+#define UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AUnitedPlanetsGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AUnitedPlanetsGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AUnitedPlanetsGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AUnitedPlanetsGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AUnitedPlanetsGameModeBase(AUnitedPlanetsGameModeBase&&); \
+	NO_API AUnitedPlanetsGameModeBase(const AUnitedPlanetsGameModeBase&); \
+public:
+
+
+#define UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AUnitedPlanetsGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AUnitedPlanetsGameModeBase(AUnitedPlanetsGameModeBase&&); \
+	NO_API AUnitedPlanetsGameModeBase(const AUnitedPlanetsGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AUnitedPlanetsGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AUnitedPlanetsGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AUnitedPlanetsGameModeBase)
+
+
+#define UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_12_PROLOG
+#define UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_SPARSE_DATA \
+	UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_RPC_WRAPPERS \
+	UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_INCLASS \
+	UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_SPARSE_DATA \
+	UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> UNITEDPLANETS_API UClass* StaticClass<class AUnitedPlanetsGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID UnitedPlanets_Source_UnitedPlanets_UnitedPlanetsGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ RPG/UnitedPlanets/Intermediate/Build/Win64/UnitedPlanetsEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/TheK-B1000/Desktop/The K-B Folder/United Planets of Galaxies/RPG/UnitedPlanets/Intermediate/Build/Win64/UnitedPlanetsEditor/Development/Engine/SharedPCH.Engine.h"
