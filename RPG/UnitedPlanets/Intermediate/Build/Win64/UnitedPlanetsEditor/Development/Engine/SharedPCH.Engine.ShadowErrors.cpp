a424da2c17025176ed2a9c33f5acd834e5c2af73
DESKTOP-9MUFES4\K-B <k-b0514@hotmail.com>
2020-08-05 23:58:13 -0700

    89 - Delete corrupt data

--- RPG/UnitedPlanets/Intermediate/Build/Win64/UnitedPlanetsEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/TheK-B1000/Desktop/The K-B Folder/United Planets of Galaxies/RPG/UnitedPlanets/Intermediate/Build/Win64/UnitedPlanetsEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- UnitedPlanets-master/RPG/UnitedPlanets/Intermediate/Build/Win64/UnitedPlanetsEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/K-B/Desktop/K-B Folder/UnitedPlanets/UnitedPlanets-master/RPG/UnitedPlanets/Intermediate/Build/Win64/UnitedPlanetsEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
